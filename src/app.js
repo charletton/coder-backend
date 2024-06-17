63f5ed6c9439b6243e70ddd8d55ef2f1bf1fb325
charletton <emitalque@gmail.com>
2024-06-17 19:17:37 -0300

    nice

--- src/app.js
+++ src/app.js
@@ -1,23 +1,32 @@
-//imports + inicalizar app
-import express from 'express';
-import cartRouter from './routes/cart.routes.js'
-import productsRouter from './routes/products.routes.js'
+//imports
+import __dirname from './utils.js';
+import productsRouter from './routes/products.routes.js';
+import cartRouter from './routes/cart.routes.js';
 
-//app
-const app = express()
+import express from 'express';
+import handlebars from 'express-handlebars';
+import {Server} from 'socket.io';
+
+// app
+const app = express();
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () => console.log(`server deployed! on ${PORT}`));;
+const socketServer = new Server(server);
+
+//handlebars config
+app.engine('handlebars',handlebars.engine());
+app.set('views', `${__dirname}/views`);
+app.set('view engine','handlebars');
 
 //permitir leer json + ruta de static
-app.use(express.static('./src/public'))
-app.use(express.json())
+app.use(express.static(`${__dirname}/public`));
+app.use(express.json());
 
 //routes
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartRouter);
-
 app.use('/', (req,res) => {
-  res.send('Welcome to the main page!')
-})
+    res.render('index');
+});
 
-//port + deploy
-const PORT = process.env.PORT || 8080
-app.listen(PORT, () => console.log(`server deployed! on ${PORT}`))
+
